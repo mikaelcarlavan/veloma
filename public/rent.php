dd2de5d9a4cb74ab54fe674cdb1d1f18e9c239df
Mikael Carlavan <contact@mika-carl.fr>
2022-10-25 20:49:57 +0200

    Update rent.php

--- public/rent.php
+++ public/rent.php
@@ -227,7 +227,7 @@ if ($id > 0) {
                 </div>
                 <div class="mt-2 py-3 sm:flex sm:flex-row-reverse">
                     <button type="submit" class="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"><?php echo $langs->trans('VelomaValidate'); ?></button>
-                    <a href="<?php echo dol_buildpath('/veloma/public/rent.php', 1); ?>" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"><?php echo $langs->trans('VelomaCancel'); ?></a>
+                    <a href="<?php echo dol_buildpath('/veloma/public/rent.php?mode='.$mode, 1); ?>" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"><?php echo $langs->trans('VelomaCancel'); ?></a>
                 </div>
             </form>
         </div>
